3362a40b04beca01317000d32887857c100eaee4
Instalacion de Softawres <software@ulsa-noroeste.edu.mx>
2019-02-28 08:51:14 -0700

    SHADER X 100000

--- Plantilla OpenGL/Shader.cpp
+++ Plantilla OpenGL/Shader.cpp
@@ -54,9 +54,54 @@ glShaderSource(fragmentShaderID, 1, &cadenaCodigoFragment, NULL);
 glCompileShader(vertexShaderID);
 glCompileShader(fragmentShaderID);
 
+//4.- Verificar errores de compilacion
+verificarCompilacion(vertexShaderID);
+verificarCompilacion(fragmentShaderID);
+
+//5.- adjuntar shaders al programa
+glAttachShader(shaderID, vertexShaderID);
+glAttachShader(shaderID, fragmentShaderID);
+
+//6.- Vincular programa
+glLinkProgram(shaderID);
+
+//7.- Verificar la vinculacion
+verificarVinculacion(shaderID);
+
+//8.- Usar el programa
+glUseProgram(shaderID);
+
 }
 
-void Shader::verificarCompilacion(GLuint id) {
+void shader::verificarCompilacion(GLuint id) {
 	GLint resultado = GL_FALSE;
-	int longitudLog = 0;
+	GLint longitudLog = 0;
+
+	glGetShaderiv(id, GL_COMPILE_STATUS, &resultado);
+	glGetShaderiv(id, GL_INFO_LOG_LENGTH, &longitudLog);
+
+	if (longitudLog > 0) {
+		vector<char> mensajeError(longitudLog);
+		glGetShaderInfoLog(id, longitudLog, NULL, &mensajeError[0]);
+		for (vector<char>::const_iterator i = mensajeError.begin();
+			i != mensajeError.end();
+			i++) {
+			cout << *i;
+		}
+	}
+}
+
+void shader::verificarVinculacion(GLuint id) {
+	GLint estadoVinculacion, estadoValidacion;
+
+	glGetProgramiv(id, GL_LINK_STATUS, &estadoVinculacion);
+	if (estadoVinculacion == GL_FALSE) {
+		cout << "No se pudo vincular el programa" << endl;
+	}
+
+	glGetProgramiv(id, GL_VALIDATE_STATUS, &estadoValidacion);
+	if (estadoValidacion == GL_FALSE) {
+		cout << "No se pudo validar tu vinculacion" << endl;
+	}
+
 }
